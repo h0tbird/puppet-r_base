75f6b10643ba5c3b8a3126dd6747566cb05e4b35
Marc Villacorta <marc.villacorta@gmail.com>
2014-10-25 20:47:25 +0200

    Adding recurse parameter

--- manifests/terminfo/common.pp
+++ manifests/terminfo/common.pp
@@ -2,8 +2,9 @@ class r_base::terminfo::common inherits r_base {
 
   if $terminfo {
     class { '::terminfo':
-      purge  => $terminfo['purge'],
-      backup => $terminfo['backup'],
+      purge   => $terminfo['purge'],
+      backup  => $terminfo['backup'],
+      recurse => $terminfo['recurse'],
     }
   }
 }
