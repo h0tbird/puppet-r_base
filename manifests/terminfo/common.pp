acc8f6800b81a7d1dfaadf8a4d2188e1da767eac
Marc Villacorta <marc.villacorta@gmail.com>
2014-10-25 20:42:25 +0200

    Adding backup parameter

--- manifests/terminfo/common.pp
+++ manifests/terminfo/common.pp
@@ -2,7 +2,8 @@ class r_base::terminfo::common inherits r_base {
 
   if $terminfo {
     class { '::terminfo':
-      purge => $terminfo['purge'],
+      purge  => $terminfo['purge'],
+      backup => $terminfo['backup'],
     }
   }
 }
